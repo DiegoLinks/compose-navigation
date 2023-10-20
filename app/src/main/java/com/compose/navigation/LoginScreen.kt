7a2ec9eb3ddcc325781f4187361e4f9720d2b406
Diego <diego.rof@hotmail.com>
2023-10-20 13:02:41 -0300

    Episódio 01 - Abrir tela de Login com navigation
    Observação: Fiz a alteração do nome do tema de "ComposeNavigationTheme" para "NavigationTheme", mas isso não tem relação com o tutorial. Foi apenas pra garantir que o código aqui fique igual ao do vídeo.

---
+++ app/src/main/java/com/compose/navigation/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.compose.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen(){
+    Text(text = "Você está na tela de login")
+}
--- app/src/main/java/com/compose/navigation/MainActivity.kt
+++ app/src/main/java/com/compose/navigation/MainActivity.kt
@@ -3,44 +3,25 @@ package com.compose.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.compose.navigation.ui.theme.ComposeNavigationTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.compose.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeNavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            NavigationTheme {
+
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "login") {
+                    composable(route = "login") {
+                        LoginScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeNavigationTheme {
-        Greeting("Android")
     }
 }
